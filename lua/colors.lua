dc527d79b749c706a1e4b223a6dca5441419af96
pacokwon <haechank@gmail.com>
2021-04-05 09:15:24 +0900

    feat(inverse): custom colorscheme inverse option

--- lua/colors.lua
+++ lua/colors.lua
@@ -2,6 +2,8 @@
 local cmd = vim.cmd
 local g = vim.g
 
+g.visual_inverse = 1
+
 -- ====== vim-solarized8 ======
 g.solarized_visibility = 'high'
 g.solarized_extra_hi_groups = 1
@@ -25,4 +27,8 @@ cmd 'colorscheme srcery'
 
 if g.colors_name == 'srcery' then
     cmd 'highlight! link Pmenu PmenuThumb'
+end
+
+if g.visual_inverse == 1 then
+    cmd 'highlight! Visual gui=reverse'
 end
