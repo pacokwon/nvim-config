117af330c5c424adf7ca89047665310bd5930c2a
pacokwon <haechank@gmail.com>
2021-06-08 23:54:11 +0900

    refactor(gitsigns): change reset hunk binding

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,7 +8,7 @@ require'gitsigns'.setup {
 
         ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
         ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-        ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+        ['n <leader>hx'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
         ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
     },
     watch_index = {
