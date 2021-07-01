c7d62213f55073da8addd234ffa80066aeac18b6
pacokwon <haechank@gmail.com>
2021-07-01 17:50:02 +0900

    feat: on_attach for metals

--- lua/plugins/metals.lua
+++ lua/plugins/metals.lua
@@ -1,9 +1,13 @@
+local metals = require'metals'
 local cmd = vim.cmd
 
-metals_config = require'metals'.bare_config
+metals_config = metals.bare_config
 metals_config.init_options.statusBarProvider = 'on'
+metals_config.on_attach = function(client, bufnr)
+  print("'" .. client.name .."' language server started");
+end
 
 cmd [[augroup lsp]]
 cmd [[au!]]
-cmd [[au FileType scala,sbt lua require("metals").initialize_or_attach(metals_config)]]
+cmd [[au FileType scala,sbt lua require'metals'.initialize_or_attach(metals_config)]]
 cmd [[augroup end]]
