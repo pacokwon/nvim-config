8a921209e68d958472f35d6194c65128998ead36
pacokwon <haechank@gmail.com>
2021-07-19 14:54:11 +0900

    refactor: use 'single' border for lsp hover doc

--- lua/plugins/metals.lua
+++ lua/plugins/metals.lua
@@ -1,10 +1,12 @@
-local metals = require'metals'
+local metals = require 'metals'
 local cmd = vim.cmd
 
 metals_config = metals.bare_config
 metals_config.init_options.statusBarProvider = 'on'
 metals_config.on_attach = function(client, bufnr)
   print("'" .. client.name .."' language server started");
+  vim.lsp.handlers["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, { border = 'single' })
+  vim.lsp.handlers["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.hover, { border = 'single' })
 end
 
 cmd [[augroup lsp]]
