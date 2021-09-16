2704acc4d41f205b39a406fd1fdbe6a356944e13
pacokwon <haechank@gmail.com>
2021-09-16 14:46:52 +0900

    Visual binding for hunk staging

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -7,6 +7,7 @@ require'gitsigns'.setup {
         ['n ('] = { expr = true, "&diff ? '(' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'" },
 
         ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+        ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({ vim.fn.line("."), vim.fn.line("v") })<CR>',
         ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
         ['n <leader>hx'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
         ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
@@ -19,5 +20,4 @@ require'gitsigns'.setup {
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil, -- Use default
-    use_internal_diff = true,  -- If luajit is present
 }
