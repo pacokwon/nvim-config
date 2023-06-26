28e6572253b53bdff140179cabaf7f8ad5428e60
pacokwon <haechank@gmail.com>
2023-06-27 00:30:10 +0900

    Enable incremental selection for treesitter

--- lua/lazy-plugins/treesitter.lua
+++ lua/lazy-plugins/treesitter.lua
@@ -89,6 +89,15 @@ return {
                 enable = true,
                 extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
                 max_file_lines = nil, -- Do not enable for files with more than n lines, int
+              },
+              incremental_selection = {
+                enable = true,
+                keymaps = {
+                  init_selection = "<Enter>",
+                  node_incremental = "<Enter>",
+                  scope_incremental = "<C-S>",
+                  node_decremental = "<BS>",
+                },
               }
             }
         end,
